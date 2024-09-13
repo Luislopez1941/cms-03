f71fa380b124263063f0f68bd4cbeb50ec272606
Luislopez1941 <jorg3luislopezc1941@hotmail.com>
2024-09-13 16:17:10 -0500

    commit

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -11,13 +11,9 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/* LEFT */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
-        <Link
-          href="/"
-          className="flex items-center justify-center lg:justify-start gap-2"
-        >
-          <Image src="/logo.png" alt="logo" width={32} height={32} />
-          <span className="hidden lg:block font-bold">SchooLama</span>
+      <div className="w-[16%] md:w-[10%] lg:w-[17%] xl:w-[14%] md:p-4 p-2">
+        <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
+          <p className="font-extrabold tracking-widest text-gray-500">JLLC</p>
         </Link>
         <Menu />
       </div>
